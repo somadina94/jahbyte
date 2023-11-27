e4db57270fb0e8c624dcf79d00aaf2ca0da0773f
unknown <somadinao@protonmail.com>
2023-11-27 23:19:04 +0100

    testing

--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -1,5 +1,6 @@
 import { CgMenuGridO } from 'react-icons/cg';
 import { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import classes from './Header.module.css';
 import Nav from './Nav';
@@ -7,13 +8,19 @@ import Nav from './Nav';
 const Header = () => {
   const navRef = useRef(null);
 
+  const naviagte = useNavigate();
+
   const handleShowNav = () => {
     navRef.current.classList.toggle(classes.display);
   };
 
+  const navigateHome = () => {
+    naviagte('/');
+  };
+
   return (
     <header className={classes.header}>
-      <h2>JahByte</h2>
+      <h2 onClick={navigateHome}>JahByte</h2>
       <CgMenuGridO className={classes.menu} onClick={handleShowNav} />
       <div className={classes.nav} ref={navRef}>
         <Nav />
